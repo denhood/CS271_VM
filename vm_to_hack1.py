8ec927407d873534af200e6bcf773f318c6cacae
smithh05 <smithh05@mycu.concord.edu>
2016-02-26 13:16:24 -0500

    Added pop

--- vm_to_hack1.py
+++ vm_to_hack1.py
@@ -20,7 +20,11 @@ def setup(fileName): #setup puts starting address of stack in SP
     fileName.write("@256\n"+\
                    "D=A\n"+\
                    "@SP\n"+\
-                   "M=D")
+                   "M=D\n"+\
+                   "@300\n"+\
+                   "D=M\n"+\
+                   "@LCL\n"+\
+                   "M=D\n")
     return
     
 def push(memSeg, offset): #NOTE: only complete segment is constant
@@ -99,8 +103,26 @@ def push(memSeg, offset): #NOTE: only complete segment is constant
     else:
         return ('seg not found')
 
-def pop():
-    pass
+def pop(memSeg, offset):
+    if memSeg == 'constant':
+        return('\n@SP'+\
+               'M=M-1')
+    elif memSeg == 'static':
+        return('\n@SP'+\
+               '\nM=M-1'+\
+               '\n@'+memSeg+\
+               '\nD=M'+\
+               '\n@'+str(offset)+\
+               '\nD=A+D'+\
+               '\n@R15'+\
+               '\nM=D'+\
+               '\n@SP'+\
+               '\nD=M'+\
+               '\n@R15'+\
+               '\nA=M'+\
+               '\nM=D')
+               
+    
 
 def add():
     #add the last two values on the stack
@@ -332,6 +354,8 @@ def VM_command_to_HACK(instruction,file):
     templst = instruction.split()
     if templst[0] == 'push':
         write_to_file(push(templst[1],templst[2]),file)
+    elif templst[0] == 'pop':
+        write_to_file(pop(templst[1],templst[2]),file) 
     elif templst[0] == 'add':
         write_to_file(add(),file)
     elif templst[0] == 'sub':
@@ -357,5 +381,6 @@ def main():
     fptr = open_file("vmout.asm")
     setup(fptr) #setup address of stack
     #parse file
+    VM_command_to_HACK('pop local 3', fptr)
     close_file(fptr)
     print("file written")
